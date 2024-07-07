2fc6d7df12200d7a90c249ea7755f166143485cd
qBlackbirdp <blackbird9711@gmail.com>
2024-07-07 18:52:41 +0900

    articleController Class 도입

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
+
 public class App {
 
     static Scanner sc;
@@ -36,16 +37,16 @@ public class App {
 
             switch (first) {
                 case "write":
-                    doWrite();
+                    ArticleController.doWrite();
                     break;
                 case "list":
-                    showList();
+                    ArticleController.showList();
                     break;
                 case "delete":
                     try {
                         if (id.isEmpty()) {
                             System.out.println("게시물 번호 입력해");
-                        } else doDelete(Integer.parseInt(id));
+                        } else ArticleController.doDelete(Integer.parseInt(id));
                     } catch (NumberFormatException e) {
                         System.out.println("게시물 번호 숫자로 입력해");
                     }
@@ -54,7 +55,7 @@ public class App {
                     try {
                         if (id.isEmpty()) {
                             System.out.println("게시물 번호 입력해");
-                        } else doModify(Integer.parseInt(id));
+                        } else ArticleController.doModify(Integer.parseInt(id));
                     } catch (NumberFormatException e) {
                         System.out.println("게시물 번호 숫자로 입력해");
                     }
@@ -66,67 +67,5 @@ public class App {
         }
     }
 
-    private static void doModify(int id) {
-        Article found = foundArticleId(id);
-        if (found == null) {
-            System.out.printf("%d번 게시물 없음.\n", id);
-        } else {
-            System.out.println("제목 : " + found.getTitle());
-            System.out.println("내용 : " + found.getBody());
-            System.out.print("새 제목 : ");
-            String newTitle = sc.nextLine().trim();
-            System.out.print("새 내용 : ");
-            String newBody = sc.nextLine().trim();
 
-            System.out.printf("%d번 게시물 수정됨.\n", id);
-
-            found.setTitle(newTitle);
-            found.setBody(newBody);
-        }
-    }
-
-    private static void doDelete(int id) {
-        Article found = foundArticleId(id);
-        if (found != null) {
-            articles.remove(found);
-            System.out.println("== 게시물 삭제 ==");
-            System.out.printf("%d번 게시물 삭제\n", id);
-        } else System.out.printf("%d번 게시물 없음.\n", id);
-    }
-
-    private static Article foundArticleId(int id) {
-        for (Article article : articles) {
-            if (article.getId() == id) {
-                return article;
-            }
-        }
-        return null;
-    }
-
-    private static void showList() {
-        System.out.println("== 게시물 목록 ==");
-        if (articles.isEmpty()) {
-            System.out.println("게시물 없어.");
-        } else {
-            System.out.println("번호       /       제목      /       내용");
-            for (int i = articles.size() - 1; i >= 0; i--) {
-                Article article = articles.get(i);
-                System.out.printf("%d       /       %s      /       %s\n", article.getId(), article.getTitle(), article.getBody());
-            }
-        }
-    }
-
-    private static void doWrite() {
-        System.out.println("== 게시물 작성 ==");
-        System.out.print("제목 : ");
-        String title = sc.nextLine().trim();
-        System.out.print("내용 : ");
-        String body = sc.nextLine().trim();
-
-        int id = lastId++;
-
-        articles.add(new Article(id, title, body));
-
-        System.out.printf("%d번 게시물이 작성되었습니다.\n", id);
-    }
 }
