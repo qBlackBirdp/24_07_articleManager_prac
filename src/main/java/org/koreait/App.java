bcacd0f05ce9fa4167e32c36eac9cc20be15e6ff
qBlackbirdp <blackbird9711@gmail.com>
2024-07-07 19:05:11 +0900

    게시물작성구현중 게시물 저장 준비

--- src/main/java/org/koreait/App.java
+++ src/main/java/org/koreait/App.java
@@ -1,12 +1,15 @@
 package org.koreait;
 
+import java.io.PrintStream;
 import java.util.Scanner;
 
 public class App {
     static Scanner sc;
+    static int lastId;
 
     public App() {
         sc = new Scanner(System.in);
+        lastId = 1;
     }
 
     static void run() {
@@ -17,12 +20,29 @@ public class App {
             String cmd = sc.nextLine().trim();
             if (cmd.equals("exit")) {
                 System.out.println("== 프로그램 종료 ==");
-            } else System.out.println("없는 명령어");
+                return;
+            }
 
             switch (cmd) {
                 case "write":
+                    doWrite();
+                    break;
+                default:
+                    System.out.println("없는 명령어");
                     break;
             }
         }
+    }
+
+    private static void doWrite() {
+        System.out.println("== 게시물 작성 ==");
+        System.out.print("제목 : ");
+        String title = sc.nextLine().trim();
+        System.out.print("내용 : ");
+        String body = sc.nextLine().trim();
+
+        int id = lastId++;
+
+        System.out.printf("%d번 게시물 작성완료\n", id);
     }
 }
